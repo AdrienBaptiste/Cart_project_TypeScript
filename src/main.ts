4de2e9109b31313c24e0797fd0b78c27aa48ff18
Adrien BAPTISTE <adrienbaptiste@live.fr>
2024-09-29 19:57:51 +0200

    conflict resolving

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,10 @@ import { Cart } from './services/Cart';
 import { InMemoryStorage } from './services/InMemoryStorage';
 import { Product } from './Entities/Product';
 
-// Insta,cie le stockage en mémoire
 const stockage = new InMemoryStorage();
 
-// Instancie le panier en lui passant l'instance de stockage
 const cart = new Cart(stockage);
 
-// Création de plusieurs produits
 const baguette = new Product('Baguette', 120);
 const croissant = new Product('Croissant', 423);
 const pain = new Product('Pain', 835);
@@ -24,3 +21,6 @@ console.log("Prix total du panier :", cart.getTotalPrice());
 cart.resetCart();
 cart.addProduct(croissant);
 console.log("Prix total du panier :", cart.getTotalPrice());
+
+console.log("bonjour")
+console.log("salut")
